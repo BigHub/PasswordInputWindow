b2b1a1f18ce99b1c76ff4a2468a2e54c6dfa1cb0
Jeffrey.Luo <jianwei@outlook.com>
2015-07-28 20:06:48 +0800

    Initial Commit

---
+++ PasswordInputWindow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PasswordInputWindow
+//
+//  Created by jianwei on 15/7/28.
+//  Copyright (c) 2015年 Jianwei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
